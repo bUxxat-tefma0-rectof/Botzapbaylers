3c38e71d7bf4573a9525c4beba90ab3f754db80a
Joaodaniel11telegram <pedrovasco263@gmail.com>
2026-07-24 13:12:43 -0300

    Refactor idGenerator.js to streamline ID generation
    Refactor idGenerator.js to simplify functions and reduce code size.

--- src/utils/idGenerator.js
+++ src/utils/idGenerator.js
@@ -1,33 +1,12 @@
 const { v4: uuidv4 } = require('uuid');
 
-function generateId() {
-    return uuidv4();
-}
+function generateId() { return uuidv4(); }
 
 function generateShortId(length = 8) {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
-    for (let i = 0; i < length; i++) {
-        result += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
+    for (let i = 0; i < length; i++) result += chars.charAt(Math.floor(Math.random() * chars.length));
     return result;
 }
 
-function generateNumericId(length = 6) {
-    let result = '';
-    for (let i = 0; i < length; i++) {
-        result += Math.floor(Math.random() * 10);
-    }
-    return result;
-}
-
-function generateTimestampId() {
-    return Date.now().toString(36).toUpperCase();
-}
-
-module.exports = {
-    generateId,
-    generateShortId,
-    generateNumericId,
-    generateTimestampId
-};
+module.exports = { generateId, generateShortId };
